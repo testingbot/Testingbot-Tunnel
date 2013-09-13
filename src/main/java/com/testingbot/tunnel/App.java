34f41e9c718adfb7b0504d83b5cb02a656e3b358
Jochen <info@testingbot.com>
2013-09-13 15:40:04 +0200

    bump version number to 1.13

--- src/main/java/com/testingbot/tunnel/App.java
+++ src/main/java/com/testingbot/tunnel/App.java
@@ -20,7 +20,7 @@ import ssh.SSHTunnel;
 import ssh.TunnelPoller;
 
 public class App {
-    public static final String VERSION = "1.12";
+    public static final String VERSION = "1.13";
     private Api api;
     private String clientKey;
     private String clientSecret;
