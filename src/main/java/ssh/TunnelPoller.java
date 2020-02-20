4429fcd74e4a25abca13e7f5722c7d57165ef330
jochen <info@testingbot.com>
2020-02-20 10:54:20 +0100

    tunnel poller: don't poll indefinitely

--- src/main/java/ssh/TunnelPoller.java
+++ src/main/java/ssh/TunnelPoller.java
@@ -16,6 +16,7 @@ public class TunnelPoller {
     private final App app;
     private final Timer timer;
     private final String tunnelID;
+    private int pollerCount = 0;
     
     public TunnelPoller(App app, String tunnelID) {
         this.app = app;
@@ -29,19 +30,29 @@ public class TunnelPoller {
     }
     
     class PollTask extends TimerTask {
+        int counter = 0;
+        
+        @Override
         public void run() {
             Api api = app.getApi();
             JSONObject response;
             try {
                 response = api.pollTunnel(tunnelID);
+                
+                if (this.counter > 80) {
+                    Logger.getLogger(TunnelPoller.class.getName()).log(Level.SEVERE, "Unable to create tunnel, waited for 400 seconds. Please try again or check https://status.testingbot.com");
+                    timer.cancel();
+                }
             
                 if (response.getString("state").equals("READY")) {
                    timer.cancel();
                    app.tunnelReady(response);
                 } else {
+                    this.counter += 1;
                     Logger.getLogger(TunnelPoller.class.getName()).log(Level.INFO, "Current tunnel status: {0}", response.getString("state"));
                 }
             } catch (Exception ex) {
+                timer.cancel();
                 Logger.getLogger(TunnelPoller.class.getName()).log(Level.SEVERE, "Unable to poll for tunnel status.");
             }
         }
